c2e706c69c5b97cb58571ed8a04fc0108ebe118f
cabasa2425 <carlosbarretosantana@gmail.com>
2025-04-23 19:44:16 +0200

    cambiando bcrypt a js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const pug = require("pug");
 
